321ec9d98c4a54ed54db5f82bd45c94d007c940b
ashi-github <162344371+ashi-github@users.noreply.github.com>
2024-07-13 12:00:44 +0000

    Day 1 Completed

---
+++ day1.js
@@ -0,0 +1,73 @@
+// ----task 1----
+
+var num = 5
+console.log(num);
+
+// ----task 2----
+
+let str = "Hello"
+console.log(str);
+
+//  ----task 3----
+
+const bool = true
+console.log(bool);
+
+// ----task 4----
+
+let num = 4
+let str = "hey"
+let bool = false
+const obj = {
+    name: "ashi",
+    age: 20
+}
+const arr = [1,2,3,4]
+
+console.log(typeof num, typeof str, typeof bool, typeof obj, typeof arr,);
+
+// ----task 5----
+
+let num = 3
+console.log(num);
+
+num = 5
+console.log(num);
+
+// ----task 6----
+
+const num = 1
+console.log(num);
+
+num = 3
+console.log(num);
+
+// ----feature request 1---- 
+let num = 4
+let str = "hey"
+let bool = false
+const obj = {
+    name: "ashi",
+    age: 20
+}
+const arr = [1,2,3,4]
+
+console.log(`${num} : ${typeof num}`);
+console.log(`${str} : ${typeof str}`);
+console.log(`${bool} : ${typeof bool}`);
+console.log(`${obj} : ${typeof obj}`);
+console.log(`${arr} : ${typeof arr}`);
+
+// ----feature request 2---- 
+
+let numInLet = 2
+console.log(numInLet);
+
+numInLet = 3
+console.log(numInLet);
+
+const numInConst = 2
+console.log(numInConst);
+
+numInConst = 3
+console.log(numInConst);
